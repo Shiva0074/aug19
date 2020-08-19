7b1f9446095796156280f2075702de81769b5767
Shiva0074 <65110424+Shiva0074@users.noreply.github.com>
2020-08-19 23:05:20 +0530

    Add files via upload

---
+++ mybot.py
@@ -0,0 +1,69 @@
+import logging
+import os
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+PORT = int(os.environ.get('PORT', '8443'))
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+
+
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
+
+
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def main():
+    """Start the bot."""
+
+    APP_NAME = os.environ.get('APP_NAME')
+    TOKEN = os.environ.get('TOKEN')
+
+    updater = Updater(
+        TOKEN, use_context=True)
+
+    dp = updater.dispatcher
+
+    # Handling commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    # If not Command i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_webhook(listen="0.0.0.0",
+                          port=PORT,
+                          url_path=TOKEN)
+    
+    updater.bot.set_webhook(APP_NAME + TOKEN)
+
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
